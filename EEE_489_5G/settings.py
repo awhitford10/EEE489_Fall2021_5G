20c322b70b4d589e999e2579df0cfe2cbdff1760
awhitford10 <awhitford@Andrews-Mini.home>
2021-09-24 15:47:31 -0400

    test

--- EEE_489_5G/settings.py
+++ EEE_489_5G/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#oqi^$b%0ex&@0(9($4%7-ew6m5q$#fb+x%m(!mt=bjxm*xy5w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
