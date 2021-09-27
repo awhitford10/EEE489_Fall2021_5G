7554b63ff32863c20b5f08456583fe93d83d0acc
awhitford10 <awhitford@Andrews-Mini.home>
2021-09-27 15:38:40 -0400

    image compression done

--- pi_code/pi.py
+++ pi_code/pi.py
@@ -3,19 +3,21 @@
 # url may need to be adjusted
 
 from requests import get
-import smbus
+from PIL import Image, ImageOps
+# import smbus
 import sys
 import time as t
-import board
-import adafruit_veml7700
-from picamera import PiCamera
+# import board
+# import adafruit_veml7700
+# from picamera import PiCamera
 
-camera = PiCamera()                             # camera
-i2c = board.I2C()                               # uses board.SCL and board.SDA
-veml7700 = adafruit_veml7700.VEML7700(i2c)      # light sensor
+
+# camera = PiCamera()                             # camera
+# i2c = board.I2C()                               # uses board.SCL and board.SDA
+# veml7700 = adafruit_veml7700.VEML7700(i2c)      # light sensor
 DEVICE_BUS = 1
 DEVICE_ADDR = 0x11
-bus = smbus.SMBus(DEVICE_BUS)
+# bus = smbus.SMBus(DEVICE_BUS)
 
 
 
@@ -23,28 +25,42 @@ while True:
 
     ############################# LED Section ###########################################
   
-    led_fetch_url = f'https://eee489-5g.herokuapp.com/rasberryled/{veml7700.lux}'   # fetch for lux value
+    # led_fetch_url = f'https://eee489-5g.herokuapp.com/rasberryled/{veml7700.lux}'   # fetch for lux value
+    led_fetch_url = f'https://eee489-5g.herokuapp.com/rasberryled/0'   # fetch for lux value
+    print('low lux value simulated')
     led_response = get(led_fetch_url).json()                                        # make a get request to heroku website
 
     if led_response['led_setting'] == 1:
-        bus.write_byte_data(DEVICE_ADDR, 1, 0xFF)           # turns on led if fetch request sends back on command
-        image = camera.capture(format = 'jpeg')             # Image taken if led is on
+        # bus.write_byte_data(DEVICE_ADDR, 1, 0xFF)           # turns on led if fetch request sends back on command
+        print('led turns on')
+        # image = camera.capture(format = 'jpeg')             # Image taken if led is on
+        print('takes picture')
         t.sleep(1)             
-        bus.write_byte_data(DEVICE_ADDR, 1, 0x00)           # turns off led if fetch sends back off
+        # bus.write_byte_data(DEVICE_ADDR, 1, 0x00)           # turns off led if fetch sends back off
+        print('led turns off')
+        break
 
     elif led_response['led_setting'] == 0:
-        bus.write_byte_data(DEVICE_ADDR, 1, 0x00)           # turns off led if fetch sends back off
+        # bus.write_byte_data(DEVICE_ADDR, 1, 0x00)           # turns off led if fetch sends back off
+        print('led turns off')
         t.sleep(1)  
 
 
     ############################ Image Section ##########################################
 
 
-    image_fetch_url = f'https://eee489-5g.herokuapp.com/rasberryled/{     }'   # fetch for lux value
-    image_response = get(image_fetch_url).json()
+    # image_fetch_url = f'https://eee489-5g.herokuapp.com/rasberryled/test'   # fetch for lux value
+    # image_response = get(image_fetch_url).json()
 
-    if image_response['complete'] == 1:
-        break    
+    # if image_response['complete'] == 1:
+    #     break    
 
     
     t.sleep(1)                                              # controls burst interval
+
+im = Image.open('HandsignalA.jpg')
+im = ImageOps.grayscale(im)
+im = im.resize((28,28), Image.ANTIALIAS)                    #resize image to same size as ML analysis
+
+im.save('resize.jpg')                                       #image saved to for viewing purposes 
+print('image data:',list(im.getdata()))                                   #This info needs to be pushed to fetch request
